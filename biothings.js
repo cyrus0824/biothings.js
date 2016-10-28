5d58dfd4cef3a34b7ad9b474045134f06c5ed91c
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2016-10-28 14:33:55 -0700

    Getting back to basics...

--- biothings.js
+++ biothings.js
@@ -17,10 +17,119 @@ function* batchIterator(batchString, chunkSize) {
     }
 }
 
+function runGenerator(g) {
+    var it = g(), ret;
+
+    // asynchronously iterate over generator
+    (function iterate(val){
+        ret = it.next( val );
+
+        if (!ret.done) {
+            // poor man's "is it a promise?" test
+            if ("then" in ret.value) {
+                // wait on the promise
+                ret.value.then( iterate );
+            }
+            // immediate value: just send right back in
+            else {
+                // avoid synchronous recursion
+                setTimeout( function(){
+                    iterate( ret.value );
+                }, 0 );
+            }
+        }
+    })();
+}
+
+function createAsyncFunction(fn) {
+  return function () {
+    var gen = fn.apply(this, arguments);
+    return new Promise(function (resolve, reject) {
+      function step(key, arg) {
+        try {
+          var info = gen[key](arg);
+          var value = info.value;
+        } catch (error) {
+          reject(error);
+          return;
+        }
+        if (info.done) {
+          resolve(value);
+        } else {
+          return Promise.resolve(value).then(function (value) {
+            step("next", value);
+          }, function (err) {
+            step("throw", err);
+          });
+        }
+      }
+      return step("next");
+    });
+  };
+}
+
+
+
+    async function getChunk(endpoint, queryOpts) {
+        var res;
+        const res2 = await $.post(endpoint, queryOpts).then(function (data) {
+            console.log("In here"); 
+            res = data;
+            console.log("After res");
+        });
+        console.log("Here");
+        return res;
+    }
+//function spawn(callback) {
+//  return function() {
+//    var iter = callback.apply(this, arguments);
+//
+//    return Promise.resolve().then(function onValue(lastValue){
+//      var result = iter.next(lastValue);
+//
+//      var done  = result.done;
+//      var value = result.value;
+//
+//      if (done) return value; // generator done, resolve Promise
+//      return Promise.resolve(value).then(onValue, iter.throw.bind(iter)); // repeat
+//    });
+//  };
+//}
+
+
+
+    function* spawn(generatorFunc) {
+        function continuer(verb, arg) {
+            var result;
+            try {
+                result = generator[verb](arg);
+            } catch (err) {
+                return Promise.reject(err);
+            }
+            if (result.done) {
+                return result.value;
+            } else {
+                return Promise.resolve(result.value).then(onFulfilled, onRejected);
+            }
+        }
+        var generator = generatorFunc();
+        var onFulfilled = continuer.bind(continuer, "next");
+        var onRejected = continuer.bind(continuer, "throw");
+        yield onFulfilled();
+    }
+
+    function getData(endpoint, queryOpts) {
+        return new Promise(function (resolve, reject) {
+            $.post(endpoint, queryOpts).then(resolve, reject);
+        });
+    }
+
+
 (function( biothings, $, undefined ) {
     /******************************
          Private properties
     *******************************/
+    var that = this;
 
     /******************************
          Public properties
@@ -31,10 +140,6 @@ function* batchIterator(batchString, chunkSize) {
     /******************************
          Private methods
     *******************************/
-    function getData(endpoint, query_opts) {
-        
-    }
-
     function spawn(generatorFunc) {
         function continuer(verb, arg) {
             var result;
@@ -55,6 +160,18 @@ function* batchIterator(batchString, chunkSize) {
         return onFulfilled();
     }
 
+    function getData(endpoint, queryOpts) {
+        return new Promise(function (resolve, reject) {
+            $.post(endpoint, queryOpts).then(resolve, reject);
+        });
+    }
+
+    async function getChunk(endpoint, queryOpts) {
+        let res;
+        await $.post(endpoint, queryOpts).then(function (data) {console.log("In here"); res = data;});
+        return res;
+    }
+
     /******************************
          Public methods
     *******************************/
@@ -68,8 +185,13 @@ function* batchIterator(batchString, chunkSize) {
         this.backendBatchSize = batch_size ? batch_size : 1000;
     }
 
+    //biothings.post = function* (endpoint, query_opts) {
+    //    for 
+
+
+
     // Execute a generic post
-    biothings.post = function* (endpoint, query_opts) {
+    biothings.post_helper = function* (endpoint, query_opts) {
         let queryOpts = typeof query_opts != 'undefined' ? query_opts : {};
         if (typeof endpoint != 'string') {
             throw "endpoint must be a string with the endpoint you want to post to"
@@ -79,27 +201,42 @@ function* batchIterator(batchString, chunkSize) {
             throw "query_opts must be an object containing the query description, e.g. {'q': 'cdk2', 'fields': 'entrezgene'}"
         }
         let iterableKey = endpoint == 'query' ? 'q' : 'ids';
-        let thisEndpoint = this.url + endpoint;
+        let thisEndpoint = this.biothings.url + endpoint;
         let theseIds = queryOpts[iterableKey];
-        let batches = batchIterator(theseIds, this.backendChunkSize);
+        let batches = batchIterator(theseIds, this.biothings.backendBatchSize);
         let thisChunk = '';
+        var chunkRes;
 
         delete queryOpts[iterableKey];
+
+        //while (true) {
 
         while (thisChunk = batches.next()) {
             if (thisChunk.done) {break;}
             let thisData = queryOpts;
             thisData[iterableKey] = thisChunk.value;
-            $.when(
-                $.post(thisEndpoint, thisData, function(data) {console.log("callback");console.log(data); console.log("AG");})).then( function yieldDocs(data, textStatus, jqXHR) {
-                    
+            yield getData(thisEndpoint, thisData);
             
-                for (var i=0; i<data.length; i++) {
-                    console.log(data[i]);
-                    return data[i];
-                }
-            });
+            //res.then(function(result) {
+            //    chunkRes = result;
+            //});
+            //return res;
+            //res.then(function (result) {
+            //    console.log("TUTU");
+            //    for (var i=0; i<result.length; i++) {
+            //        yield result[i];
+            //    }
+            //});
+            //res.resolve().then(function
+            //res.then({
+            //    for (var i=0; i<res.length; i++) {
+            //        yield res[i];
+            //    }
+            //});
+            //console.log("TT"); 
+            //for (var i=0; i<res.length; i++) {
+            //    yield res[i];
+            //}
         }
     }
-
 }( window.biothings = window.biothings || {}, jQuery));
